88f23b20d3744324b59d2312e8db92f6340e7d26
syedshaon <syedshaon99@gmail.com>
2024-07-13 16:35:59 +0600

    changed output format of categories, tags and added excerpt

--- src/settings.js
+++ src/settings.js
@@ -2,13 +2,7 @@
 // Order is preserved. If a field has an empty value, it will not be included. You can rename a
 // field by providing an alias after a ':'. For example, 'date:created' will include 'date' in
 // frontmatter, but renamed to 'created'.
-exports.frontmatter_fields = [
-	'title',
-	'date',
-	'categories',
-	'tags',
-	'coverImage'
-];
+exports.frontmatter_fields = ["title", "date", "author", "excerpt", "categories", "tags", "coverImage"];
 
 // Time in ms to wait between requesting image files. Increase this if you see timeouts or
 // server errors.
@@ -25,15 +19,15 @@ exports.include_time_with_date = false;
 // Override post date formatting with a custom formatting string (for example: 'yyyy LLL dd').
 // Tokens are documented here: https://moment.github.io/luxon/#/parsing?id=table-of-tokens. If
 // set, this takes precedence over include_time_with_date.
-exports.custom_date_formatting = '';
+exports.custom_date_formatting = "";
 
 // Specify the timezone used for post dates. See available zone values and examples here:
 // https://moment.github.io/luxon/#/zones?id=specifying-a-zone.
-exports.custom_date_timezone = 'utc';
+exports.custom_date_timezone = "utc";
 
 // Categories to be excluded from post frontmatter. This does not filter out posts themselves,
 // just the categories listed in their frontmatter.
-exports.filter_categories = ['uncategorized'];
+exports.filter_categories = ["uncategorized"];
 
 // Strict SSL is enabled as the safe default when downloading images, but will not work with
 // self-signed servers. You can disable it if you're getting a "self-signed certificate" error.
